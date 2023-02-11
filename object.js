e95e2b2554a486ad73cd81bf6ed2bed086fbbc54
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2023-02-11 08:02:05 +0600

    Practice Object.

---
+++ object.js
@@ -0,0 +1,14 @@
+// JavaScript Object
+
+let MyObject = {
+    firstName: 'sahinur',
+    lastName: 'salafee',
+    age: 26,
+    height: '5.9inc',
+    weight: '72kg'
+}
+
+console.log(MyObject);
+
+let MyName = MyObject.firstName;
+console.log(MyName);
