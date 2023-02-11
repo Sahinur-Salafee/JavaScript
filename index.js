a0213bf1efa51607f502e770f1956c12b2af0f0a
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2023-02-11 06:49:57 +0600

    Practice Array Functions

--- index.js
+++ index.js
@@ -4,8 +4,24 @@ let students = ['Rahim','Latif','Tomal'];
 console.log(students.length);
 
 let studentOne = students[0];
-document.write(students);
+document.write(students + '<br>');
 
 for(let i=0; i<students.length; i++) {
-    console.log('Students Name: ' + students[i]);
+    document.write('Students Name: ' + students[i] + '<br>');
 }
+
+// Insert element start of the array
+let addStudents = students.unshift('Tanin');
+console.log(students);
+
+// Insert element of the array
+let addStudent = students.push('Touki');
+console.log(students);
+
+// Remove element start of the array
+let removeStudent = students.shift();
+console.log(students);
+
+// Remove element of the array
+let removeStudents = students.pop();
+console.log(students);
