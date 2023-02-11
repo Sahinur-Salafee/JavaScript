79bbab5f304ce39d9d24309f721cb6bd34dd3f2f
salafee-xs <72187069+salafee-xs@users.noreply.github.com>
2023-02-11 06:12:27 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/* Array */
+
+let students = ['Rahim','Latif','Tomal'];
+console.log(students.length);
+
+let studentOne = students[0];
+document.write(students);
+
+for(let i=0; i<students.length; i++) {
+    console.log('Students Name: ' + students[i]);
+}
