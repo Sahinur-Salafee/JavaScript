9df572cad0196f9cc1c3d3d7591afad2cf72696e
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2023-02-12 06:57:15 +0600

    Practice Object.

--- object.js
+++ object.js
@@ -5,10 +5,28 @@ let MyObject = {
     lastName: 'salafee',
     age: 26,
     height: '5.9inc',
-    weight: '72kg'
+    weight: '72kg',
+    welcomeMsg: function() {
+        console.log('Welcome to my site.');
+    }
 }
 
 console.log(MyObject);
 
 let MyName = MyObject.firstName;
 console.log(MyName);
+
+// Add new value in object
+MyObject.zip = 1207;
+
+// Add new method in object
+MyObject.greeting = function() {
+    console.log('Hello There');
+}
+
+MyObject.welcomeMsg();
+
+// Access all the object properties
+for(item in MyObject) {
+    console.log(item);
+}
