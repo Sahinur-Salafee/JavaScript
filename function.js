e83836441a82ac21ebd1eb79e37e8f8bc96c4a0d
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2023-02-16 07:04:15 +0600

    Practice functions.

--- function.js
+++ function.js
@@ -40,3 +40,15 @@ var a = 100;
 console.log('Value of a before changing function is applied: ' + a);
 changeValue(a);
 console.log('Value of a after changing function is applied: ' + a);
+
+
+function aFunc(a) {
+    console.log('Before changing the property: ' + a.one);
+    a.one = 'two';
+    console.log('After changing the property: ' + a.one);
+}
+
+var a = {
+    one: 'one'
+}
+aFunc(a);
