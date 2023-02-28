ff0eb47bc5d5d606fd8c0325ca4703821ffd240b
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2023-02-28 07:06:57 +0600

    Practice Hoisting.

---
+++ hoisiting.js
@@ -0,0 +1,22 @@
+function greetings() {
+    hello();
+    console.log('Hello Salafee');
+}
+
+function hello() {
+    console.log('Hello World');
+}
+
+greetings();
+
+add();
+
+function add() {
+    console.log('This is a add function');
+}
+
+animal();
+
+let animal = function() {
+    console.log('This is a animal function');
+}
