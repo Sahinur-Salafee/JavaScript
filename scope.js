1938530c14b878b1fd3e19690bbbe858f792c2ba
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2023-03-01 09:37:50 +0600

    Practice Scoping.

--- scope.js
+++ scope.js
@@ -10,7 +10,6 @@
 
 function localscope() {
     var local = 'Local Scope';
-
     console.log(local);
 }
 
@@ -45,3 +44,109 @@ function anotherGlobalFunc() {
 }
 
 anotherGlobalFunc();
+
+// Object Scoping
+
+var student = {
+    name: 'salafee',
+    age: 27
+}
+
+console.log(student);
+
+function ObjFunc() {
+    var students = {
+        name: 'mahbub',
+        age: 28
+    }
+
+    console.log(students);
+}
+
+ObjFunc();
+
+// console.log(students);
+
+
+/* 
+    Auto Global Variable
+*/
+
+function GlobalVar() {
+    name = 'salafee';
+    console.log(name);
+}
+
+GlobalVar();
+
+console.log(name);
+
+
+/* 
+    Lexical Scoping
+*/
+
+function parentFunction() {
+    var a = 10;
+
+    function childFunction() {
+        var b = 20;
+        console.log('Sum ' + (a + b));
+    }
+
+    childFunction();
+}
+
+parentFunction();
+
+
+var x = 'mahbub';
+
+function mahbub() {
+    var x = 'salafee';
+    console.log('Inside function ' + x);  
+}
+
+mahbub();
+
+console.log('Outside function ' + x);
+
+
+function testParent() {
+
+    var x = 'Mahbub';
+
+    function testChild() {
+        var y = 'salafee';
+        console.log(x + ' knows ' + y);
+    }
+
+    testChild();
+}
+
+testParent();
+
+
+var testObj = {
+    name : 'Mahbub',
+    age: 27
+}
+
+console.log(testObj);
+
+function TestFunc() {
+    console.log(testObj);
+
+    var testOb = {
+        name : 'Salafee',
+        age: 27
+    }
+    console.log(testOb);
+}
+
+TestFunc();
+
+
+
+
+
