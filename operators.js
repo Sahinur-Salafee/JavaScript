725fc645b6cd1e9e328d928d2bccdef5d1575192
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2023-03-05 04:53:56 +0600

    Practice operators

---
+++ operators.js
@@ -0,0 +1,48 @@
+
+var x = 4;
+
+var y = x ** x; //  y = x^x
+console.log(y);    
+
+let a = 10;  // int
+
+// == value same or not
+
+// === value + type same or not 
+
+if(a === '10') { // string
+    console.log('true');
+} else {
+    console.log('false');
+}
+
+
+let n = 10;
+
+if(n == 10 || n== 2) {
+    console.log('True');
+} else {
+    console.log('false');
+}
+
+// true && true = true
+
+// true && false = false
+
+// false && false = true
+
+// false && true = false
+
+// true || true = true
+
+// true || false = true
+
+// false || true = true
+
+// false || false = false
+
+
+// (true && false) || (false || true) 
+//       false || true
+//          true
+
