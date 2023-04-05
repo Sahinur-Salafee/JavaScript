fe8a1b997f59862d65bd7884e9401b0c27504e3c
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2023-04-05 06:36:18 +0600

    Practice ES6 IFFE.

---
+++ ES6/IIFE.js
@@ -0,0 +1,23 @@
+(function msg() {
+    var msg = "Hello World";
+    console.log(msg);
+})();
+
+{
+    let a = 10;
+    let b = 20;
+    let pi = 3.1416;
+    console.log(a + b + pi);
+}
+
+// console.log(pi); show error
+
+{
+    let showName = function () {
+        console.log('My name is Salafee');
+    }
+
+    showName();
+}
+
+// showName(); show error
