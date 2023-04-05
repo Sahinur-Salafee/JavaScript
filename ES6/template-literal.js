5da28d0fc429b7e265e091c7623574f636f81222
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2023-04-05 06:47:24 +0600

    Practice Template Literal.

---
+++ ES6/template-literal.js
@@ -0,0 +1,13 @@
+let name = 'Salafee';
+let age = 27;
+let job = 'Web Developer';
+
+console.log(`My name is ${name} and I am ${age} years old. I am a ${job}`);
+
+
+// For function
+
+function ageCalculator(dob) {
+    console.log(`You are ${2023 - dob} years old`);
+}
+ageCalculator(1996);
