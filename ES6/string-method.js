4aad09efeba1106e57eeded0ce19dca34da01ce8
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2023-04-06 06:26:45 +0600

    String Methods

---
+++ ES6/string-method.js
@@ -0,0 +1,21 @@
+// String Methods
+
+let a = "My country name is Bangladesh";
+
+// Includes Method
+console.log(a.includes("Bangladesh")); // true
+
+// StartsWith Method
+console.log(a.startsWith("My")); // true
+
+// EndsWith Method
+console.log(a.endsWith("Bangladesh")); // true
+
+// Repeat Method
+console.log("Hello ".repeat(5)); // Hello Hello Hello Hello Hello
+console.log(`I want to say  ${'Alhamdulillah\n'.repeat(10)}`);
+
+// Trim Method
+let b = "   Hello World   ";
+console.log(b.trim()); // Hello World
+
