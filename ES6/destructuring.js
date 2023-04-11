973c93690779c647fd89dac8e04a511b24f1a619
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2023-04-11 06:16:54 +0600

    Destructuring in JS.

--- ES6/destructuring.js
+++ ES6/destructuring.js
@@ -1,5 +1,7 @@
 // Destructuring
 
+// Array
+
 let salafee = ['Salafee', 27, 'WordPress Developer'];
 
 // ES5
@@ -15,3 +17,23 @@ let [name2, age2, job2] = salafee;
 console.log(name2);
 console.log(age2);
 console.log(job2);
+
+
+// Destructuring in Object
+
+// ES5
+const myObj = {
+    name: 'salafee',
+    age: 27,
+    job: 'Web Developer'
+}
+
+var name1 = myObj.name;
+var age1 = myObj.age;
+console.log(name1);
+console.log(age1);
+
+// ES6 
+const { name: name3, age: age3, job: job3 } = myObj;
+console.log(name3);
+console.log(age3);
