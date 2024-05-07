6b0e71ce660b25f3bd1b90cd5d4ebb04f7f4dc45
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2024-05-07 06:16:13 +0600

    Object and factory function.

---
+++ oop/object.js
@@ -0,0 +1,30 @@
+const circle = {
+    radius: 1,
+    location: {
+        x:1,
+        y:2
+    },
+    draw: function() {
+        console.log('Draw');
+    }
+}
+
+circle.draw();
+console.log(circle.location.x);
+
+/* Factory Function */
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function () {
+            console.log('Draw');
+        }
+    }
+}
+
+const circle2 = createCircle(2);
+console.log(circle2);
+console.log(circle2.draw());
+
+const circle3 = createCircle(3);
+console.log(circle3);
