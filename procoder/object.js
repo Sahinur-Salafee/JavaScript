f474a10c77d4f83e802104737419b0989bf1d818
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2024-05-16 06:44:31 +0600

    Practice Object.

--- procoder/object.js
+++ procoder/object.js
@@ -18,8 +18,16 @@ const mobileModel = {
 const mobileId = Symbol('id');
 console.log(mobileId);
 mobileModel[mobileId] = 548;
+Object.freeze(mobileModel); // Freeze any key
+mobileModel.model = 's25 Ultra';
 console.log(mobileModel);
 console.log(mobileModel.brand);
 console.log(mobileModel['model']);
 console.log(mobileModel[newSym]);
 console.log('Mobile ID: ' + mobileModel[mobileId]);
+
+console.log(mobileModel.hasOwnProperty('camera'));
+// Return Object keys as Array
+console.log(Object.keys(mobileModel));
+// Return Object values as Array
+console.log(Object.values(mobileModel));
